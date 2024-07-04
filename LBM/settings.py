72ac7411c77a93ff111c144376ea2da276916c42
Rk-Krishna <142603393+Rk-Krishna@users.noreply.github.com>
2024-07-04 10:39:56 +0530

    Update settings.py

--- LBM/settings.py
+++ LBM/settings.py
@@ -30,7 +30,7 @@ EMAIL_HOST_PASSWORD="akra msev bltb gexd"
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
